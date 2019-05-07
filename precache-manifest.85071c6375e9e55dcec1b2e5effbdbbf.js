dc31400e60a1c36f81a86b7b73fb5fa166634a43
Domante Stirbyte <domante.stirbyte@gmail.com>
2019-05-07 09:35:10 +0200

    Updates

---
+++ precache-manifest.85071c6375e9e55dcec1b2e5effbdbbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cc2173119a1a79f385695d65f099c3",
+    "url": "/city-tours/index.html"
+  },
+  {
+    "revision": "2a93dc137f66972e3da8",
+    "url": "/city-tours/static/css/main.dde6f38f.chunk.css"
+  },
+  {
+    "revision": "e9cacbf2bc110e26e9e8",
+    "url": "/city-tours/static/js/2.dfb0cf60.chunk.js"
+  },
+  {
+    "revision": "2a93dc137f66972e3da8",
+    "url": "/city-tours/static/js/main.7c7f2ea6.chunk.js"
+  },
+  {
+    "revision": "7f782f880009c1695655",
+    "url": "/city-tours/static/js/runtime~main.6b8a86ca.js"
+  }
+]);
